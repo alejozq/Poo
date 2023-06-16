fcb382612b4e83ee2b92ede31b90039498502539
alejozq <73606260+alejozq@users.noreply.github.com>
2023-06-16 16:35:39 -0500

    Add files via upload

---
+++ Actividad5/src/Ejercicio83/Principal.java
@@ -0,0 +1,10 @@
+
+package Ejercicio83;
+public class Principal {
+   public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; 
+        miVentanaPrincipal= new VentanaPrincipal(); 
+        miVentanaPrincipal.setVisible(true); 
+        miVentanaPrincipal.setResizable(false);
+    } 
+}
