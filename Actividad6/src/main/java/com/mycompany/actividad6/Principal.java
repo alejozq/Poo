b5b2fab383ae8ec0751c92b9f0f278dd396393ba
alejozq <73606260+alejozq@users.noreply.github.com>
2023-06-25 20:54:00 -0500

    Add files via upload

---
+++ Actividad6/src/main/java/com/mycompany/actividad6/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividad6;
+
+/**
+ *
+ * @author Tactical
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        Frame1 ins1= new Frame1();
+        ins1.setVisible(true);
+    }
+}
